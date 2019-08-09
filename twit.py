85b9d0cf0850b6354e360fe4d503701b8cf32699
acurious-designer <50954166+sunc-dev@users.noreply.github.com>
2019-08-08 20:34:34 -0400

    Initial Commit

--- twit.py
+++ twit.py
@@ -1,17 +1,20 @@
 import twint
-
 #Enter Twitter Handler
 
 
 def user_input():
     handler = input("Enter a twitter handler: ")
-    return handler
+    start = input("Enter a date to start the search: ")
+    end = input("Enter a date to end the search: ")
+    return handler, start, end
 
 
 def handlers(handler):
     handlers = []
     handlers.append(handler)
+    return handler
 
 
-print(user_input())
-print(handlers(user_input())
+c = twint.Config()
+c.Username = user_input()
+print(handlers(c.Username))
