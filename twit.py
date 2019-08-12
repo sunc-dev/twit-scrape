fbd569c178f92b6d3dd683bb08c22fd33e0c5790
Chris Sun <christopher.sun.sun@gmail.com>
2019-08-12 08:44:16 -0400

    update

--- twit.py
+++ twit.py
@@ -6,7 +6,9 @@ This is a temporary script file.
 """
 
 import twint
+import pandas as pd
 import os
+import json
 import nest_asyncio
 nest_asyncio.apply()
 #Enter Twitter Handler
@@ -14,13 +16,16 @@ nest_asyncio.apply()
 
 def set_path():
     path = r"C:\Users\csunj\OneDrive\Documents\Github\Repository\twit-scrape"
-    dl = os.path.join(path, 'data')
-    return dl
+    fName = 'tweets.json'
+    fPath = os.path.join(path, 'data', fName)
+    return fPath,fName
 
 
 def user_input():
     handler = input("Enter a twitter handler: ")
-    return handler
+    start = input("Enter a date for Tweets since: ")
+    end = input("Enter a date for Tweets until: ")
+    return handler, start, end
 
 
 def handlers(handler):
@@ -29,16 +34,29 @@ def handlers(handler):
     return handlers
 
 
-def get_tweets(handler, dl):
+def get_tweets(handler, fPath):
     c = twint.Config()
     c.Username = handler
+    c.Store_json = True
+    c.Retweets = True
+    c.Limit = 1
+    c.Hide_output=True
+    c.Output = fPath
+    twint.run.Search(c)
 
-    c.Store_csv = True
-    c.Limit = 10
-    c.Output = os.path.join(dl, handler + ".csv")
-    twint.run.Profile(c)
+def load_tweets(fPath):
+    tweets=[]
+    for line in open(fPath, 'r',encoding='utf8'):
+        tweets.append(json.loads(line))
+    return tweets
 
 
-handler = user_input()
-dl = set_path()
-get_tweets(handler, dl)
+def parse_tweets(tweets):
+    data=pd.DataFrame(tweets)
+    return data
+
+handler, start, end = user_input()
+fPath, fName = set_path()
+get_tweets(handler, fPath)
+tweets = load_tweets(fPath)
+data = parse_tweets(tweets)
