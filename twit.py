7c433919e5538e37b11e002b2f8acb6236f446a1
Chris Sun <christopher.sun.sun@gmail.com>
2019-08-16 09:03:56 -0400

    Updates

--- twit.py
+++ twit.py
@@ -21,6 +21,7 @@ def user_input():
     end = input("Enter a date for Tweets until: ")
     return handler, start, end
 
+
 def set_paths():
     directory = r"C:\Users\csunj\OneDrive\Documents\Github\Repository\twit-scrape"
     fileName = 'account_list.csv'
@@ -28,49 +29,49 @@ def set_paths():
     acctPath = os.path.join(path, fileName)
     return directory, acctPath, path, fileName
 
+
 def load_users(acctPath):
     accounts = pd.read_csv(acctPath)
-   
+
     with open(acctPath, 'r') as f:
         reader = csv.reader(f)
         l = list(reader)
         accounts = [val for sublist in l for val in sublist]
     return accounts
 
+
 def get_tweets(path, i):
     c = twint.Config()
     c.Username = i
     c.Store_json = True
     c.Retweets = True
-    c.Limit = 1
-    c.Hide_output=True
+    c.Hide_output = True
     c.Output = path
     twint.run.Search(c)
 
+
 def search_tweets():
-      for i in accounts:
-          get_tweets(path, i)
-    
+    for i in accounts:
+        get_tweets(path, i)
+
+
 def load_tweets(path):
-    tweets=[]
-    for line in open(os.path.join(path,'tweets.json'), 'r',encoding='utf8'):
+    tweets = []
+    for line in open(os.path.join(path, 'tweets.json'), 'r', encoding='utf8'):
         tweets.append(json.loads(line))
     return tweets
 
+
 def parse_tweets(tweets):
-    data=pd.DataFrame(tweets)
+    data = pd.DataFrame(tweets)
     return data
-             
+
+
 def clean_data(data):
-    cols = ['mentions'
-            ,'urls'
-            ,'photos'
-            ,'cashtags'
-            ,'hashtags']
-    data[cols] = data[cols].astype(str).replace(
-        r"\[|\]|\['|\']|'", 
-        '', 
-        regex=True)
+    cols = ['mentions', 'urls', 'photos', 'cashtags', 'hashtags']
+    data[cols] = data[cols].astype(str).replace(r"\[|\]|\['|\']|'",
+                                                '',
+                                                regex=True)
     return data
 
 
@@ -80,6 +81,3 @@ search_tweets()
 tweets = load_tweets(path)
 data = parse_tweets(tweets)
 data = clean_data(data)
-
-
-
